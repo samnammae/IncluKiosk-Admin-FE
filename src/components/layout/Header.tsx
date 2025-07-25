f04ed33c632d1a5b788479331aa0c09a43d72228
kdhqwe1030 <kdhqwe1030@naver.com>
2025-07-26 01:30:06 +0900

    fix: 로그인 안한 경우 대시보드 진입 불가 처리

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { useLoginModalStore } from "@/lib/store/loginStore";
 import { authAPI } from "@/lib/api/auth";
+import { useEffect } from "react";
 
 export default function Header() {
   const { isLoggedIn, setIsLoggedIn, openLoginModal } = useLoginModalStore();
@@ -18,14 +19,24 @@ export default function Header() {
       console.error("Login failed:", error);
     }
   };
+  useEffect(() => {
+    console.log(isLoggedIn);
+  }, []);
   return (
     <>
       <header className="w-full h-16 flex justify-between items-center px-4">
         <div className="">IncluKiosk</div>
         <div className="flex gap-8">
-          <Link href={"/dashboard"}>
-            <div className="">대시보드</div>
-          </Link>
+          {isLoggedIn ? (
+            <Link href={"/dashboard"}>
+              <div className="">대시보드</div>
+            </Link>
+          ) : (
+            <div className="" onClick={handleLoginClick}>
+              대시보드
+            </div>
+          )}
+
           {isLoggedIn ? (
             <div className="" onClick={handleLogoutClick}>
               로그아웃
