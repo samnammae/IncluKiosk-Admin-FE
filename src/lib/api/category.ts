dea8b9438428a999f4e9deaad9d796c1cc5f67ac
kdhqwe1030 <kdhqwe1030@naver.com>
2025-07-29 21:32:57 +0900

    feat: 카테고리 api 구현

---
+++ src/lib/api/category.ts
@@ -0,0 +1,34 @@
+import api from "./api";
+
+export const categoryAPI = {
+  addCategory: async (shopId: number, data: addCategoryData) => {
+    const response = await api.post(`/menu/${shopId}/category`, data);
+    console.log("카테고리 추가", response.data);
+    return response.data;
+  },
+  getAllCategory: async (shopId: number) => {
+    const response = await api.get(`/menu/${shopId}/category`);
+    console.log("카테고리 조회", response.data);
+    return response.data;
+  },
+  updateCategory: async (shopId: number, data: updateCategoryData[]) => {
+    const response = await api.put(`/menu/${shopId}/category`, data);
+    console.log("카테고리 수정", response.data);
+    return response.data;
+  },
+  deleteCategory: async (shopId: number, categoryId: string) => {
+    const response = await api.delete(`/menu/${shopId}/category/${categoryId}`);
+    console.log("카테고리 삭제", response.data);
+    return response.data;
+  },
+};
+
+export interface addCategoryData {
+  name: string;
+  displayOrder: number;
+}
+export interface updateCategoryData {
+  id: string;
+  name: string;
+  displayOrder: number;
+}
