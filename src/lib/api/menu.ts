4fb8d0dced520808a4288bedd81caa71fd1ee3d8
kdhqwe1030 <kdhqwe1030@naver.com>
2025-08-19 07:19:12 +0900

    feat: 메뉴 생성, 수정, 삭제 api 구현

--- src/lib/api/menu.ts
+++ src/lib/api/menu.ts
@@ -6,4 +6,27 @@ export const menuAPI = {
     console.log("메뉴 조회", response.data);
     return response.data;
   },
+  addMenu: async (shopId: number, fromData: FormData) => {
+    const response = await api.post(`/menu/${shopId}`, fromData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    console.log("메뉴 생성", response.data);
+    return response.data;
+  },
+  updateMenu: async (shopId: number, menuId: string, fromData: FormData) => {
+    const response = await api.put(`/menu/${shopId}/${menuId}`, fromData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    console.log("메뉴 수정", response.data);
+    return response.data;
+  },
+  deleteMenu: async (shopId: number, menuId: string) => {
+    const response = await api.delete(`/menu/${shopId}/${menuId}`);
+    console.log("메뉴 삭제", response.data);
+    return response.data;
+  },
 };
