8c94df1922f609abf624ee2273d2204bc88086db
kdhqwe1030 <kdhqwe1030@naver.com>
2025-08-24 16:04:05 +0900

    feat: 페이지 기본 레이아웃 구현  및 페이지 용도 변경(매출통계 -> 옵션관리)

--- src/app/dashboard/option/page.tsx
+++ src/app/dashboard/option/page.tsx
@@ -1,3 +1,14 @@
+import OptionManage from "@/components/layout/option/OptionManage";
+import TitleHeader from "@/components/ui/title/TitleHeader";
+
 export default function OptionPage() {
-  return <div>매출 통계</div>;
+  return (
+    <div className="max-w-8xl mx-auto p-2">
+      <TitleHeader
+        title={"옵션 관리"}
+        subText={"메뉴 옵션 그룹과 개별 옵션을 관리하고 가격을 설정하세요"}
+      />
+      <OptionManage />
+    </div>
+  );
 }
