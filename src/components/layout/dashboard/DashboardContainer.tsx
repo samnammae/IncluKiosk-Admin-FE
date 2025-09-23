cae715459fbf2de67dbec590a551d9d9e083a2b3
kdhqwe1030 <kdhqwe1030@naver.com>
2025-09-23 12:39:57 +0900

    feat: 시간대별 매출 조회 차트 구현 및 API 연결

--- src/components/layout/dashboard/DashboardContainer.tsx
+++ src/components/layout/dashboard/DashboardContainer.tsx
@@ -1,16 +1,13 @@
 "use client";
 import { SectionTitle } from "@/components/ui/title/SectionTitle";
-import { dashboardAPI } from "@/lib/api/dashboard";
-import { useShopStore } from "@/lib/store/shopStore";
-import React, { useEffect } from "react";
+import React from "react";
 import CategoryChart from "./CategoryChart";
+import { useRouter } from "next/navigation";
+import HourlyChart from "./HourlyChart";
 
 const DashboardContainer = () => {
-  const { choosedShop } = useShopStore();
-  useEffect(() => {
-    if (choosedShop !== null)
-      dashboardAPI.getCategory(choosedShop!.storeId, "count");
-  }, []);
+  const router = useRouter();
+
   return (
     <div className="max-w-8xl mx-auto p-2 space-y-6">
       <div className="bg-white shadow-sm rounded-xl p-6">
@@ -19,6 +16,7 @@ const DashboardContainer = () => {
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
         <div className="bg-white shadow-sm rounded-xl p-6 lg:col-span-2">
           <SectionTitle title={"🕐 시간대별 매출"} />
+          <HourlyChart />
         </div>
         <div className="bg-white shadow-sm rounded-xl p-6 lg:col-span-1">
           <SectionTitle title={"📚 카테고리별 매출"} />
@@ -26,7 +24,14 @@ const DashboardContainer = () => {
         </div>
       </div>
       <div className="bg-white shadow-sm rounded-xl p-6">
-        <SectionTitle title={"📋 최근 주문 기록"} />
+        <SectionTitle
+          title={"📋 최근 주문 기록"}
+          buttonText="전체 보기    >"
+          buttonClassName="px-2 py-1 text-sm"
+          buttonClick={() => {
+            router.push("/dashboard/shop");
+          }}
+        />
       </div>
     </div>
   );
