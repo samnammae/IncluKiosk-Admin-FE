08cd98654e98582d19b36cdeb184bd19e9169bee
Kimdohyun <115572203+kdhqwe1030@users.noreply.github.com>
2025-09-25 19:52:23 +0900

    Revert "feat(#29): 주문관리 페이지 UI 구현 및 기능 연결"

--- src/app/dashboard/orders/page.tsx
+++ src/app/dashboard/orders/page.tsx
@@ -1,15 +1,3 @@
-import RecentTable from "@/components/layout/table/RecentTable";
-import TitleHeader from "@/components/ui/title/TitleHeader";
-
 export default function OrdersPage() {
-  return (
-    <div className="max-w-8xl mx-auto p-2">
-      {/* 헤더 */}
-      <TitleHeader
-        title={"주문 관리"}
-        subText={"최근 주문 내역을 확인할 수 있어요"}
-      />
-      <RecentTable />
-    </div>
-  );
+  return <div>주문 관리</div>;
 }
