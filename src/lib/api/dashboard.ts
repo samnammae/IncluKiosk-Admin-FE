cfeca614e21ba7319c84c04bb191ba02f952a740
kdhqwe1030 <kdhqwe1030@naver.com>
2025-09-29 15:57:58 +0900

    fix: api 타겟 라우팅 변경

--- src/lib/api/dashboard.ts
+++ src/lib/api/dashboard.ts
@@ -1,8 +1,7 @@
 import axios from "axios";
-import api from "./api";
 
 const nextApi = axios.create({
-  baseURL: `/api`,
+  baseURL: `/internal-api`,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
